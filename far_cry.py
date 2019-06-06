97e9f96d66c740602fb55959999f0963b8dd46bd
nhuong <n16dcat026@student.ptithcm.edu.vn>
2019-06-07 00:01:49 +0700

    .

--- far_cry.py
+++ far_cry.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 from datetime import datetime
 from datetime import tzinfo, timezone, timedelta
+import csv
 
 def read_log_file(log_file_pathname):
     """
     Way point 1:
-    Read and return all the bytes from the server log file.
+    Read Game Session Log File.
+    Return all the bytes from the server log file.
     @param:
     log_file_pathname: pathname of a Far Cry server log file.
     """
@@ -66,16 +68,15 @@ def parse_session_mode_and_map(log_data):
     return (parse_string[6], parse_string[4].split('/')[1].rstrip(','))
 
 
-def full_frag_time(log_data, frag_time):
+def full_frag_time(log_data, frag_time, frag_hour, frag_start_time):
     """
     Return full time of frag which is a datetime object with timezone.
     """
-    frag_start_time = parse_log_start_time(log_data, dict_cvar)
     frag_min = int(frag_time.split(':')[0])
     frag_sec = int(frag_time.split(':')[1])
-    if frag_min > frag_start_time.minute:
-        frag_hour = frag_start_time.hour + 1
-    return frag_start_time.replace(minute=frag_min, second=frag_sec).isoformat()
+    if frag_min < frag_start_time.minute:
+        frag_hour += 1
+    return frag_start_time.replace(hour=frag_hour, minute=frag_min, second=frag_sec).isoformat()
 
 def parse_frags(log_data):
     """
@@ -88,21 +89,93 @@ def parse_frags(log_data):
     (frag_time, killer_name, victim_name, weapon_code) or
     (frag_time, killer_name) if suicide.
     """
-    list_frags = []
+    frags = []
     split_log_data = log_data.split('\n')
+    frag_start_time = parse_log_start_time(log_data, dict_cvar)
+    frag_hour = frag_start_time.hour
     for line in split_log_data:
         if 'killed' in line:
-            line = line.split(' ')
-            frag_time=full_frag_time(log_data, line[0].lstrip('<').rstrip('>'))
-            if len(line) > 5:
-                list_frags.append((frag_time, line[2], line[4], line[6]))
+            line = line.split(' killed ')
+            killer_name = line[0].split(' <Lua> ')[1]
+            time = line[0].split(' <Lua> ')[0]
+            frag_time=full_frag_time(log_data, time.lstrip('<').rstrip('>'), frag_hour, frag_start_time)
+            if 'itself' not in line:
+                victim_name = line[1].split(' with ')[0]
+                weapon_code = line[1].split(' with ')[1]
+                frags.append((frag_time, killer_name, victim_name, weapon_code))
             else:
-                list_frags.append((frag_time, line[2]))
-    return list_frags
+                frags.append((frag_time, killer_name))
+    return frags
+
+
+def prettify_frags(frags):
+    """
+    Way point 7:
+    Prettify Frag History.
+    Return list of frag strings with emojis.
+    @param:
+    frags: an array of tuples of frags parsed from a Far Cry server's log file.
+    """
+    prettified_frags = []
+    emoji_dict = {'Vehicle': '🚙',
+                  'Falcon': '🔫', 'Shotgun': '🔫',
+                  'P90': '🔫', 'MP5': '🔫',
+                  'M4': '🔫', 'AG36': '🔫', 'OICW': '🔫',
+                  'SniperRifle': '🔫', 'VehicleMountedAutoMG': '🔫',
+                  'M249': '🔫', 'MG': '🔫',
+                  'VehicleMountedMG': '🔫',
+                  'HandGrenade': '💣', 'AG36Grenade': '💣',
+                  'OICWGrenade': '💣', 'StickyExplosive': '💣',
+                  'Rocket': '🚀', 'VehicleMountedRocketMG': '🚀',
+                  'VehicleRocket': '🚀',
+                  'Machete': '🔪',
+                  'Boat': '🚤'}
+    for frag in frags:
+        if len(frag) > 2:
+            string = '[' + frag[0] + ']' + ' 😛  ' + frag[1] + ' ' + emoji_dict[frag[3]] + '  😦  ' + frag[2]
+            prettified_frags.append(string)
+        else:
+            string = '[' + frag[0] + ']' + ' 😦  ' + frag[1] + ' ' + '☠'
+            prettified_frags.append(string)
+    return prettified_frags
+
+
+def parse_match_start_and_end_times(log_data):
+    """
+    Return the approximat start and end time of the game session.
+    """
+    log_start_time = parse_log_start_time(log_data, dict_cvar)
+    split_log_data = log_data.split('\n')
+    for line in split_log_data:
+        if 'loaded in' in line:
+            loading_time = int(float(line.split()[5]))
+            break
+    for line in split_log_data:
+        if 'Statistics' in line:
+            end_min = int(line.split()[0].split(':')[0].lstrip('<'))
+            end_sec = int(line.split()[0].split(':')[1].rstrip('>'))
+            break
+    start_time = log_start_time.second + loading_time
+    return (str(log_start_time.replace(second=start_time)), str(log_start_time.replace(minute=end_min, second=end_sec)))
+
+
+def write_frag_csv_file(log_file_pathname, frags):
+    """
+    Store frag history in a CSV file.
+    """
+    with open(log_file_pathname, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for frag in frags:
+            spamwriter.writerow(frag)
 
 
 log_data = read_log_file('./logs/log05.txt')
 dict_cvar = create_dictionary(log_data)
 print (parse_log_start_time(log_data, dict_cvar))
 print (parse_session_mode_and_map(log_data))
-print (parse_frags(log_data))
+frags = parse_frags(log_data)
+prettified_frags = prettify_frags(frags)
+print ('\n'.join(prettified_frags))
+start_time, end_time = parse_match_start_and_end_times(log_data)
+print(start_time, end_time)
+write_frag_csv_file('./logs/log05.csv', frags)
